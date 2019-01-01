ee707f0188d1bfe67f8f2d04ef2105b7d827aefb
sultansidhu <sultan.sidhu@mail.utoronto.ca>
2019-01-01 11:09:32 -0500

    implement forward propagation, actor-critic and LSTM

--- model.py
+++ model.py
@@ -22,20 +22,60 @@ def normalized_columns_initializer(weights, std=1.0):
     return output
 
 
-def weights_init(nn):
+def weights_init(n):
     """Initializes weights of the neural network nn based on optimal rates of learning."""
-    class_name = nn.__class__.__name__
+    class_name = n.__class__.__name__
     if class_name.find('Conv') != -1:  # for the convolution layers
-        weight_shape = list(nn.weight.data.size())
+        weight_shape = list(n.weight.data.size())
         fan_in = numpy.prod(weight_shape[1:4])
         fan_out = numpy.prod(weight_shape[2:4])*weight_shape[0]
         w_bound = numpy.sqrt(6. / fan_in+fan_out)
-        nn.weight.data.uniform_(-w_bound, w_bound)
-        nn.bias.data.fill_(0)
+        n.weight.data.uniform_(-w_bound, w_bound)
+        n.bias.data.fill_(0)
     elif class_name.find('Linear') != -1:  # for full connection layers
-        weight_shape = list(nn.weight.data.size())
+        weight_shape = list(n.weight.data.size())
         fan_in = weight_shape[1]
         fan_out = weight_shape[0]
         w_bound = numpy.sqrt(6. / fan_in + fan_out)
-        nn.weight.data.uniform_(-w_bound, w_bound)
-        nn.bias.data.fill_(0)
+        n.weight.data.uniform_(-w_bound, w_bound)
+        n.bias.data.fill_(0)
+
+# Making the brain of the A3C model
+
+
+class ActorCritic(torch.nn.Module):
+    """ Class managing the actor-critic dynamic of the A3C agent. Initializes actor-critic and the LSTM cell. """
+
+    def __init__(self, num_inputs, action_space):
+        super(ActorCritic, self).__init__()
+        self.conv1 = nn.Conv2d(num_inputs, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.lstm = nn.LSTMCell(32*3*3, 256)  # enables the neural network to understand temporal relationships
+        num_outputs = action_space.n
+        self.critic_linear = nn.Linear(256, 1)  # the V(s) for the state
+        self.actor_linear = nn.Linear(256, num_outputs)   # the Q(s, a) for the state
+        # initializing weights
+        self.apply(weights_init)
+        self.actor_linear.weight.data = normalized_columns_initializer(self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(self.critic_linear.weight.data, 1)
+        self.critic_linear.bias.data.fill_(0)
+        # small std. deviation for actor and large for critic as defined above make a
+        # good balance of exploration vs exploitation
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+        self.train()
+
+    def forward(self, inputs):
+        """The forward propagation through the A3C neural network."""
+        inputs, (hx, cx) = inputs
+        x = func.elu(self.conv1(inputs))
+        x = func.elu(self.conv2(x))
+        x = func.elu(self.conv3(x))
+        x = func.elu(self.conv4(x))
+        x = x.view(-1, 32*3*3)
+        (hx, cx) = self.lstm(x, (hx, cx))
+        x = hx
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
